6fd784d392bf06bf4cc46a4970ad7fec259b2b1e
jakeranallo <jake.ranallo@gmail.com>
2019-07-19 13:29:57 +1000

    dotenv

--- src/index.js
+++ src/index.js
@@ -5,20 +5,26 @@ import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import axios from "axios";
 
- 
+moment.locale('en-GB');
+
+require("dotenv").config()
+
 class App extends Component {
   state = {
     date: new Date(),
-    events: [],
-    isLoading: true,
-    errors: null
+    events: []
   }
 
   async getEvents() {
+
+    // Obscure this for security
+    
+    const API_KEY =`${process.env.REACT_APP_API_KEY}`
+
     let webApiUrl =
       "https://www.eventbriteapi.com/v3/users/me/events/";
     let tokenStr =
-      "ZFP4UHEKXMUCMP6RLLH5";
+      API_KEY;
 
     await axios
       .get(webApiUrl, {
@@ -28,7 +34,25 @@ class App extends Component {
       })
 
     .then(response => {
-      const eventFetch = response.data.events
+      let eventFetch = response.data.events;
+
+      // Standardise date format
+
+      for (let i = 0; i < eventFetch.length; i++) {
+        eventFetch[i].start.utc = moment.utc(eventFetch[i].start.utc).toDate();
+        eventFetch[i].end.utc = moment.utc(eventFetch[i].end.utc).toDate();}
+
+      // TODO Map items to new names in array
+
+      eventFetch.map(event => ({
+        title: `${event.name.text}`,
+        start: `${event.start.utc}`,
+        end: `${event.end.utc}`,
+        url:`${event.url}`
+      }))
+
+      // TODO Destructure to only setState with required fields
+
       this.setState({ events: eventFetch })
       console.log(this.state.events)
   })
@@ -39,27 +63,18 @@ class App extends Component {
   }
  
   render() {
-    const cal_events = []
+    const {events} = this.state
     const localizer = momentLocalizer(moment)
     return (
-      <>
-      {this.state.events.map((event) => (
-          <div key={event.id}>
-           <p>{event.name.text}</p>  
-           <p>{event.start.local}</p>  
-           <p>{event.url}</p>            
-          </div>
-        ))}
       <div style={{width:'500px', height:'500px'}}>
           <Calendar
           localizer={localizer}
-            events={cal_events}
+            events={events}
             step={30}
             defaultView='month'
             views={['month','week','day']}
             defaultDate={new Date()}/>
       </div>
-      </>
     );
   }
 }
